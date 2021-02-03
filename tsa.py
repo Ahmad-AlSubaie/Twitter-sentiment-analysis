a0d578293c302ce5c35dda17ae5790bfa5014f80
jyinja <62846048+jyinja@users.noreply.github.com>
2021-02-03 15:21:34 -0500

    added some possibly important imports

--- tsa.py
+++ tsa.py
@@ -2,5 +2,10 @@
 Jason Yin and Ahmad Alsubaie
 Twitter sentiment analysis, python code, cs499nlp
 """
-import re
-import tweepy
+import re #regular expression
+import tweepy #python client for official twitter api, install with 'pip install tweepy'
+import textblob #process text library, use 'pip install textblob' and 'python -m textblob.download_corpora'
+from textblobk import Textblob
+from tweepy import OAuthHandler #authentication
+
+
